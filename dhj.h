faed1852adb917018ef306a87ef8044238d18d79
Huangjue.deng <13136106206@163.com>
2020-04-20 14:30:55 +0800

    added io_route

--- dhj.h
+++ dhj.h
@@ -6,6 +6,103 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define ROUTE_ADD 1
+#define ROUTE_DEL 2
+
+/*
+ *  IPv4 add/del route item in route table
+    refer to https://www.cnblogs.com/wangshide/archive/2012/10/25/2740410.html
+ */
+bool io_route(int action, char *ip, char *mask, char *iface) {
+#if defined (HAVE_LINUX)
+	struct rtentry route;  /* route item struct */
+	char target[128] = {0};
+	char gateway[128] = {0};
+	char netmask[128] = {0};
+
+	struct sockaddr_in *addr;
+
+	int skfd;
+
+	/* clear route struct by 0 */
+	memset((char *)&route, 0x00, sizeof(route));
+
+	/* default target is net (host)*/
+	route.rt_flags = RTF_UP ;
+
+	if(ip) {   // default is a network target
+		strcpy(target, ip);
+		fprintf(stderr, "target: %s\n", target);
+		addr = (struct sockaddr_in*) &route.rt_dst;
+		addr->sin_family = AF_INET;
+		addr->sin_addr.s_addr = inet_addr(target);
+
+	}
+ 
+	if(mask) {   // netmask setting
+		strcpy(netmask, mask);
+		addr = (struct sockaddr_in*) &route.rt_genmask;
+		addr->sin_family = AF_INET;
+		addr->sin_addr.s_addr = inet_addr(netmask);
+
+	}
+/*
+	if(gw) {  //
+		strcpy(gateway, gw);
+		addr = (struct sockaddr_in*) &route.rt_gateway;
+		addr->sin_family = AF_INET;
+		addr->sin_addr.s_addr = inet_addr(gateway);
+		route.rt_flags |= RTF_GATEWAY;
+	}
+*/
+
+	if(iface) {  /* device setting */
+		route.rt_dev = iface;
+	}
+
+/*
+	if(mtu) {  //mtu setting
+		route.rt_flags |= RTF_MTU;
+		route.rt_mtu = atoi(*args);
+	}
+*/
+
+	/* create a socket */
+	skfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if(skfd < 0) {
+		perror("socket");
+		return false;
+	}
+
+	/* tell the kernel to accept this route */
+	if(action == ROUTE_DEL) {  /* del a route item */
+		if(ioctl(skfd, SIOCDELRT, &route) < 0) {
+			perror("SIOCDELRT");
+			close(skfd);
+			return false;
+		}
+
+	} else if(action == ROUTE_ADD) {  /* add a route item */
+		if(ioctl(skfd, SIOCADDRT, &route) < 0) {
+			perror("SIOCADDRT");
+			close(skfd);
+			return false;
+		}
+
+	} else {
+		log0("Error route action(%d)", action);	
+		return false;
+
+	}
+
+	close(skfd);
+#elif
+	fprintf(stderr, "io_route() has no effect on your ostype !!!\n");
+	
+#endif
+	return true;
+}
+
 /* get_hex:  huangjue.deng  2020.3.18
  *	while let data to get struct, 
  * 	s_t *s = malloc(sizeof(*s)); 
