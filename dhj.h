110dc3abc810d91e099f62dbbbf3a57dc5c2adb8
dhj <13136106206@163.com>
2021-07-30 11:40:10 +0800

    hex

--- dhj.h
+++ dhj.h
@@ -153,13 +153,13 @@ static inline void *xstrdup(void *data) {
  * 	printf_hex(s, sizeof(s_t));
  *
  */
-char *get_hex(void *data, size_t len) {
-	char buf[len + 1];
-	char *ret = malloc(len * 2 + 1);
-	memcpy(buf, (char *) data, len);
+uint8_t *get_hex(void *data, size_t len) {
+	uint8_t buf[len + 1];
+	uint8_t *ret = malloc(len * 2 + 1);
+	memcpy(buf, data, len);
 	memset(ret, 0, sizeof(ret));
 	for(int i = 0; i < len; i++) {
-		buf[i] = 0x00? sprintf(&ret[i * 2], "00") : (buf[i] < 0x10? sprintf(&ret[i * 2], "0%x", buf[i]) : sprintf(&ret[i * 2], "%x", buf[i]));
+		sprintf(&ret[i * 2], "%02x", buf[i]);
 	}
 
 	return ret;
@@ -172,16 +172,16 @@ char *get_hex(void *data, size_t len) {
  * 	printf_hex(s, sizeof(s_t));
  *
  */
-char *get_format_hex(void *data, size_t len) {
-	char buf[len + 1];
-	char *ret = malloc(len * 2 + len / 4 + 1);
+uint8_t *get_format_hex(void *data, size_t len) {
+	uint8_t buf[len + 1];
+	uint8_t *ret = malloc(len * 2 + len / 4 + 1);
 	memcpy(buf, (char *) data, len);
 	memset(ret, 0, sizeof(ret));
 	int count = 0;
 	int j = 0;
 	for(int i = 0; i < len; i++) {
 		j = i * 2 + count;
-		buf[i] = 0x00? sprintf(&ret[j], "00") : (buf[i] < 0x10? sprintf(&ret[j], "0%x", buf[i]) : sprintf(&ret[j], "%x", buf[i]));
+		sprintf(&ret[j], "%02x", buf[i]);
 
 		(i + 1) % 16 ? ((i + 1) % 4 ? : (sprintf(&ret[j + 2], " ") && count++)) : (sprintf(&ret[j + 2], "\n") && count++);
 	}
@@ -197,10 +197,10 @@ char *get_format_hex(void *data, size_t len) {
  *
  */
 bool printf_hex(void *data, size_t len) {
-	char buf[len + 1];
-	memcpy(buf, (char *)data, len);
+	uint8_t buf[len + 1];
+	memcpy(buf, data, len);
 	for(int i = 0; i < len; i++) {
-		buf[i] = 0x00? fprintf(stdout, "00") : (buf[i] < 0x10? fprintf(stdout, "0%x", buf[i]) : fprintf(stdout, "%x", buf[i]));
+		fprintf(stdout, "%02x", buf[i]);
 
 		(i + 1) % 16 ? ((i + 1) % 4 ? : fprintf(stdout, " ")) : fprintf(stdout, "\n");
 	}
